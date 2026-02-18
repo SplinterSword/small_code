4b872c398d5284de4e0583ac9456731dcced4935
SplinterSword <sparshj2003@gmail.com>
2026-02-18 09:41:52 +0530

    wrote rudamentory test to see if the get_file_content function tool is working properly

---
+++ test_get_file_content.py
@@ -0,0 +1,11 @@
+from functions.get_file_content import get_file_content
+
+print(get_file_content("calculator", "lorem.txt"))
+print("========================================")
+print(get_file_content("calculator", "main.py"))
+print("========================================")
+print(get_file_content("calculator", "pkg/calculator.py"))
+print("========================================")
+print(get_file_content("calculator", "/bin/cat")) # this should return an error string
+print("========================================")
+print(get_file_content("calculator", "pkg/does_not_exist.py")) # this should return an error string
